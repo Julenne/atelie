23ace2652468930b1d7ab7d2201941ba6cbbbc26
Arlene Pelenda <arlenej011@gmail.com>
2020-11-02 19:06:53 -0300

    docker

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const routes = Router();
 
 routes.post('/teste', (req,res, next) => {
-  return res.json({message: 'oioioi'})
+  return res.json({"message": 'oioioi'});
 })
 
 module.exports = routes;
